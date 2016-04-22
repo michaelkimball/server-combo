37540f30a0916f72b8388450f4241d02404e5330
Michael Kimball <mdk20@students.uwf.edu>
2016-04-22 01:36:33 -0500

    Changed exports

--- server.js
+++ server.js
@@ -4,7 +4,5 @@ var port = process.env.PORT || 80; // used to create, sign, and verify tokens
 var server = app.listen(port);
 var io = require('socket.io')(server);
 
-exports = {
-    app: app,
-    io: io
-};
+exports.app = app;
+exports.io = io;
