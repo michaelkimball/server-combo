53064340296bfaae388149dc47c52977ba6731d6
Michael Kimball <mdk20@students.uwf.edu>
2016-04-22 01:38:04 -0500

    Updated imports and commented out incomplete code

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ var tabletopsquire = ('./TableTopSquire/server');
 var madcapserver = ('./MadCap_Server/server');
 var app = server.app;
 var io = server.io;
+var path = require('path')
 var bodyParser = require('body-parser');
 var hbs = require('express-hbs');
 
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, '/MadCap_Server/views'));
+//app.set('view engine', 'hbs');
+//app.set('views', path.join(__dirname, '/MadCap_Server/views'));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
