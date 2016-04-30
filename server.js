59024380f1d987784ddd9383d4c12eea4f5ca571
Michael Kimball <mdk20@students.uwf.edu>
2016-04-29 22:16:38 -0500

    unnecessary require

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var config = require('./config');
 var hbs = require('express-hbs').create();
 var path = require('path');
 
